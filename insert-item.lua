0c819451e0d4c55e85d0b984de7cdcffd0e22f4a
Díaz Urbaneja Víctor Eduardo Diex <diaz.victor@openmailbox.org>
2019-05-13 15:40:47 -0400

    funcion setInventory() añadida.

--- insert-item.lua
+++ insert-item.lua
@@ -12,14 +12,21 @@ function player:getInventory()
     end
 end
 
+function player:setInventory(name, desc)
+    for i,v in ipairs(player.inventory) do
+        print(i)
+        id = i + 1
+    end
+    table.insert(player.inventory, id, {obj = name, desc = desc})
+end
+
 msg = 'Estas es una habitación un poco pequeña, la cual pertenecia a un periodista.' .. '\n' .. 'En el centro de la habitación hay una mesa, sobre ella se encuentra un periódico y una camara.'
 print(msg)
 
 cmd = io.read()
 
 if (cmd == 'coger camara') or (cmd == 'tomar camara') then
-    -- @TODO: Luego añadire una funcion que facilitara la cosas, leyendo la tabla player.inventory añadira el valor que queramos.
-    table.insert(player.inventory, 3, {obj = 'camara', desc = 'una camara'})
+    player:setInventory('camara', 'una camara')
     print('Cogida')
 elseif (cmd == 'inventario') then
     player:getInventory()
@@ -28,7 +35,7 @@ end
 cmd = io.read()
 
 if (cmd == 'coger periodico') or (cmd == 'tomar periodico') or (cmd == 'coger periódico') or (cmd == 'tomar periódico') then
-    table.insert(player.inventory, 4, {obj = 'periódico', desc = 'un periódico'})
+    player:setInventory('periódico', 'un periódico')
     print('Cogido')
 elseif (cmd == 'inventario') then
     player:getInventory()
